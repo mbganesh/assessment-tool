2094b7c912b5f7edf2de32806c73e2bc5a58e777
SharedPreferences <boothaganesh.jdev@gmail.com>
2022-05-27 18:37:33 +0530

    phase 2.2 [moment js]

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -8,6 +8,7 @@ import IconButton from "@mui/material/IconButton";
 import { APIClient, Colors } from "../constants";
 import { useNavigate } from "react-router-dom";
 import { Base64 } from "js-base64";
+import moment from "moment";
 
 const RootDiv = styled("div")(({ theme }) => ({
   backgroundColor: Colors.LIGHT_COLOR,
@@ -29,16 +30,41 @@ const RootCard = (theme) => ({
 });
 
 export default function LoginPage() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-    
   const [showPassword, setShowPassword] = useState(false);
 
+  const dayAndTimeChecker = () => {
+    let current =moment().format("YYYY-MM-DD HH:mm"); // current time
+    let examDayTime = "2022-05-27 17:06";
 
-  const [LoginData , setLoginData] = useState({
-      username:'',
-      password:''
-  })
+
+    let graceTime =moment(examDayTime,"YYYY-MM-DD HH:mm").add(15,'minute').format("YYYY-MM-DD HH:mm"); // grace time
+    
+
+    console.log('Current Time : ',current,'\n' , 'Exam Time: ' , examDayTime , '\n' , 'Grace Time: ' , graceTime  );
+
+    if (moment(current).isSameOrAfter(examDayTime)) {
+
+      if(moment(current).isSameOrBefore(graceTime) ){  //  time check
+          console.log('grace time');
+
+      }else{
+        console.log('exam link expired');
+      }
+
+
+    } else {
+      console.log("Exam is not started yet");
+    }
+
+
+  };
+
+  const [LoginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
@@ -47,36 +73,26 @@ export default function LoginPage() {
   };
 
   const handleLocalStorage = (data) => {
-    var dataStr = JSON.stringify(data)
-    var encode = Base64.encode(dataStr)
-    localStorage.setItem(APIClient.LOCALSTORAGE_KEY , encode)
-  }
+    var dataStr = JSON.stringify(data);
+    var encode = Base64.encode(dataStr);
+    localStorage.setItem(APIClient.LOCALSTORAGE_KEY, encode);
+  };
 
   const handleLoginBtn = () => {
+    dayAndTimeChecker();
 
     console.log(LoginData);
-      if(LoginData.username === ''){
-          alert('please enter username')
-      }else if(LoginData.password === ''){
-          alert('please enter password')
-      }else if(LoginData.password === '123'){
-
-
-        handleLocalStorage(LoginData)
-        
-
-        navigate('/home')
-      }else{
-        alert('incorrect username or password')
-      }
-  }
-
-  console.log('im redner');
-
-  useEffect( () => {
-    console.log('im in useEff');
-  } , [])
-
+    if (LoginData.username === "") {
+      alert("please enter username");
+    } else if (LoginData.password === "") {
+      alert("please enter password");
+    } else if (LoginData.password === "123") {
+      // handleLocalStorage(LoginData)
+      // navigate('/home')
+    } else {
+      alert("incorrect username or password");
+    }
+  };
 
   return (
     <div>
@@ -98,33 +114,60 @@ export default function LoginPage() {
 
           <div style={{ width: "90%", margin: "10px 0" }}>
             <Typography variant="subtitle1">UserName</Typography>
-            <TextField autoComplete="off" fullWidth size="small" variant="outlined" value={LoginData.username} onChange={(e) => setLoginData({...LoginData , username:e.target.value})}  />
+            <TextField
+              autoComplete="off"
+              fullWidth
+              size="small"
+              variant="outlined"
+              value={LoginData.username}
+              onChange={(e) =>
+                setLoginData({ ...LoginData, username: e.target.value })
+              }
+            />
           </div>
 
           <div style={{ width: "90%", margin: "10px 0" }}>
             <Typography variant="subtitle1">Password</Typography>
-            <TextField autoComplete="off" type={showPassword ? 'text' : 'password'} 
-            onKeyDown={handleKeyPress}
-            value={LoginData.password} onChange={(e) => setLoginData({...LoginData , password:e.target.value})}
-               InputProps={{
+            <TextField
+              autoComplete="off"
+              type={showPassword ? "text" : "password"}
+              onKeyDown={handleKeyPress}
+              value={LoginData.password}
+              onChange={(e) =>
+                setLoginData({ ...LoginData, password: e.target.value })
+              }
+              InputProps={{
                 endAdornment: (
-                  <InputAdornment position='end'>
+                  <InputAdornment position="end">
                     <IconButton
-                      aria-label='toggle password visibility'
-                      onClick={() => {setShowPassword(!showPassword)}}
-                      onMouseDown={(e) => e.preventDefault()}>
+                      aria-label="toggle password visibility"
+                      onClick={() => {
+                        setShowPassword(!showPassword);
+                      }}
+                      onMouseDown={(e) => e.preventDefault()}
+                    >
                       {showPassword && <Visibility />}
                       {!showPassword && <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
                 ),
               }}
-            fullWidth size="small" variant="outlined" />
+              fullWidth
+              size="small"
+              variant="outlined"
+            />
           </div>
 
           <div style={{ width: "90%", margin: "10px 0" }}>
-            <Button fullWidth size="small" variant="contained" sx={{backgroundColor:Colors.BTN_COLOR , '&:hover':{backgroundColor:Colors.BTN_COLOR}}}
-            onClick={() => handleLoginBtn()}
+            <Button
+              fullWidth
+              size="small"
+              variant="contained"
+              sx={{
+                backgroundColor: Colors.BTN_COLOR,
+                "&:hover": { backgroundColor: Colors.BTN_COLOR },
+              }}
+              onClick={() => handleLoginBtn()}
             >
               Login
             </Button>
